b414a5ecce95224dc8d94edc84a06c276a431816
stoftot <gryn@itu.dk>
2025-06-04 17:18:31 +0200

    Switching to C#

--- 40k-Roster-Master/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.w40krostermaster {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.w40krostermaster to javafx.fxml;
-    exports org.example.w40krostermaster;
-}
